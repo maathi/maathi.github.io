356ad65300d32b7644e569badaee3707ce5888f7
maathi <mahdi221b@gmail.com>
2021-02-03 08:08:42 +0100

    intro: added a card & some styling

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,18 +1,70 @@
 import pic from "../img/mathi.jpg"
 import "../style/intro.css"
+import { FaTwitter, FaGithubAlt, FaYoutube } from "react-icons/fa"
+import { MdMail } from "react-icons/md"
 
 function Intro() {
   return (
-    <div className="intro">
-      <img src={pic}></img>
-
-      <p>
-        Hey, I'm mathi, a software developer.
-        <br></br>I have a tremendous passion for solving real world problems
-        through technology. I find great joy in creating software that has the
-        potential to make people's lives somehow easier.
-      </p>
-    </div>
+    <section id="intro">
+      <div>
+        <div id="card">
+          <img src={pic}></img>
+          <p>zoomer | full stack developper | future ninja</p>
+          <ul className="icons">
+            <li>
+              <a
+                href="https://twitter.com/kitsutsukix"
+                target="_blank"
+                rel="noreferrer"
+                className="twitter"
+              >
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/maathi"
+                target="_blank"
+                rel="noreferrer"
+                className="github"
+              >
+                <FaGithubAlt />
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:mahdi221b@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+                className="mail"
+              >
+                <MdMail />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                target="_blank"
+                rel="noreferrer"
+                className="youtube"
+              >
+                <FaYoutube />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h1>
+          Hey <span id="wave">👋</span>, I'm mathi, a software developer.
+        </h1>
+        <p>
+          I have a tremendous passion for solving real world problems through
+          technology. I find great joy in creating software that has the
+          potential to make people's lives somehow easier.
+        </p>
+      </div>
+    </section>
   )
 }
 
