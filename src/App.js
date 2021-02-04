2cde691289b6207d9df9ee58cfc6fd100dcd1039
maathi <mahdi221b@gmail.com>
2021-02-04 10:35:24 +0100

    added a quote section

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Contact from "./components/contact"
 import Footer from "./components/footer"
 import Projects from "./components/projects"
 import Techs from "./components/techs"
+import Quote from "./components/quote"
 
 function App() {
   return (
     <div>
+      <Quote></Quote>
       <div className="main-content">
         <Intro></Intro>
         <Techs></Techs>
---
+++ src/components/quote.jsx
@@ -0,0 +1,17 @@
+import "../style/quote.css"
+import { FaQuoteLeft } from "react-icons/fa"
+function Quote() {
+  return (
+    <div id="quote">
+      <p>
+        <span>
+          <FaQuoteLeft />
+        </span>
+        &nbsp; Education never ends, Watson. It is a series of lessons, with the
+        greatest for the last.
+      </p>
+      <span id="author">Sherlock Holmes</span>
+    </div>
+  )
+}
+export default Quote
