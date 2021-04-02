1039e57434bcd83da7ff238130ff3520f2fb8b01
maathi <mahdi221b@gmail.com>
2021-04-02 17:02:26 +0100

    goodbye :D

--- src/App.js
+++ src/App.js
@@ -8,16 +8,21 @@ import Quote from "./components/quote"
 
 function App() {
   return (
-    <div>
-      <Quote></Quote>
-      <div className="main-content">
-        <Intro></Intro>
-        <Techs></Techs>
-        <Projects></Projects>
-        <Contact></Contact>
-      </div>
-      <Footer></Footer>
-    </div>
+    // <div>
+    //   <Quote></Quote>
+    //   <div className="main-content">
+    //     <Intro></Intro>
+    //     <Techs></Techs>
+    //     <Projects></Projects>
+    //     <Contact></Contact>
+    //   </div>
+    //   <Footer></Footer>
+    // </div>
+    <h1 style={{ textAlign: "center", margin: "4rem auto" }}>
+      I have moved my portfolio to :
+      <a href="https://mathiland.vercel.app"> https://mathiland.vercel.app</a>
+      <br></br>I rebuilt it using Next.js and added a blog section :D
+    </h1>
   )
 }
 
